0d86b0110a38cbfb6bb20a11061a65d0687c867b
Jeremy Wildfire <jwildfire@gmail.com>
2016-12-31 19:42:48 -0500

    both controls working

--- build/movieComparison.js
+++ build/movieComparison.js
@@ -209,7 +209,9 @@ var movieComparison = (function () {
 		webCharts.multiply(smallMultipleChart, longData, "movie");
 
 		//Make the controls
-		var people = ["Jeremy", "Namps!", "Sasha"];
+		var people = d3.set(longData.map(function (d) {
+			return d.person;
+		})).values();
 		controlsSettings.inputs[0].values = people;
 		controlsSettings.inputs[0].start = "Namps!";
 		controlsSettings.inputs[1].values = people;
@@ -220,16 +222,34 @@ var movieComparison = (function () {
 		console.log(controls);
 
 		var selects = controls.wrap.selectAll("div.control-group").select("select");
-
+		//Update the highlights on both charts when the controls change
 		selects.on("change", function (d) {
-			var person1 = selects[0][0].value;
-			var person2 = selects[0][1].value;
+			var name1 = selects[0][0].value;
+			var name2 = selects[0][1].value;
 
-			avgRankChart.config.person1 = person1;
-			avgRankChart.config.person2 = person2;
-			avgRankChart.config.marks[1].values.person = person1;
-			avgRankChart.config.marks[2].values.person = person2;
+			avgRankChart.config.person1 = name1;
+			avgRankChart.config.person2 = name2;
+			avgRankChart.config.marks[1].values.person = name1;
+			avgRankChart.config.marks[2].values.person = name2;
 			avgRankChart.draw();
+
+			//Highlight cells for selected reviewers
+			var mults = d3.select("div#detailChart").select("div.wc-small-multiples").selectAll("div.wc-chart");
+			console.log(mults);
+			var rects = mults.select("svg").select("g").select("g.bar-supergroup").selectAll("g.bar-group").selectAll('rect');
+
+			console.log(rects);
+			rects.attr("fill", "white");
+
+			var person1 = rects.filter(function (d) {
+				return d.key == name1;
+			});
+			person1.attr("fill", "blue");
+
+			var person2 = rects.filter(function (d) {
+				return d.key == name2;
+			});
+			person2.attr("fill", "yellow");
 		});
 	}
 
